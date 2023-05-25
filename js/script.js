61c0c4577cbbd8524cba2dae325e06913c708ef3
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-25 15:30:15 +0200

    add prompt second name user

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 console.log("JS ok")
 // check element DOM
 console.log(document.getElementById("user-password"));
-// add prompt
+// add prompt firstnameuser
 const firstnameuser = prompt("Come ti chiami?" , "Riccardo");
 // check name-user
 console.log(firstnameuser);
+// add prompt secondnameuser
+const secondnameuser = prompt("Qual'è il tuo cognome?" , "Pinto");
