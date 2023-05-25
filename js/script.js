10dc3a28a585ca01e89fa94d6fde65110e92bbb9
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-25 15:39:11 +0200

    add element firstnameuser.secondnameuser,favcoloruser in element Dom

--- js/script.js
+++ js/script.js
@@ -14,3 +14,9 @@ console.log(secondnameuser);
 const favcoloruser = prompt("Qual'è il tuo colore preferito?" , "Blu");
 // check fav-color-user
 console.log(favcoloruser);
+/*
+add element
+firstnameuser,secondnameuser and favcoloruser
+in element DOM
+*/
+document.getElementById("user-password").innerHTML = "La tua password è:" + firstnameuser + secondnameuser + favcoloruser +"21";
