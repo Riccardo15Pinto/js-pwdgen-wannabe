44ff5b353dfb401d7092731d2e283ee9f3bc4a35
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-25 15:27:29 +0200

    check firstnameuser

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// check script.js
 console.log("JS ok")
-
+// check element DOM
 console.log(document.getElementById("user-password"));
+// add prompt
+const firstnameuser = prompt("Come ti chiami?" , "Riccardo");
+// check name-user
+console.log(firstnameuser);
